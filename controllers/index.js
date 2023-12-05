10c0f409af4c3a7d87c793ef808ef89889c18ddd
Arthur Bridges <arthurbridges08@yahoo.com>
2023-12-04 18:23:59 -0600

    added routes to index.js in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,17 @@
 
 const router = require.apply('express').Router();
 const homeRoute = require('./homeRoute');
+const angelBeatsRoute = require('./api/angelBeatsRoute');
+const clannadRoute = require('./api/clannadRoute');
+const onePieceRoute = require('./api/onePieceRoute');
+const violetRoute = require('./api/violetEvergardenRoute');
 
 //router.use for all below
 
 router.use('/', homeRoute);
+router.use('/angelbeats', angelBeatsRoute);
+router.use('/clannad', clannadRoute);
+router.use('/onepiece', onePieceRoute);
+router.use('/violetevergarden', violetRoute);
 
 module.exports = router;
