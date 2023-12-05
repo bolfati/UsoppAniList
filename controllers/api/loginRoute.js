6973c5a942e3dfc64cce26b32e3a4822503aad06
SHoang6702 <shoang6702@gmail.com>
2023-12-04 19:11:32 -0600

    login, logout, signup, routes

---
+++ controllers/api/loginRoute.js
@@ -0,0 +1,49 @@
+
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Your username or password is wrong brother!" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Your email or password is wrong brother!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res
+        .status(200)
+        .json({ user: userData, message: "You have logged in brother!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
