546d32df82630e964965c916804f46eadec923ab
Arthur Bridges <arthurbridges08@yahoo.com>
2023-12-04 18:31:45 -0600

    finished the index.js routes

---
+++ controllers/api/index.js
@@ -0,0 +1,20 @@
+//All routes below
+const router = require('express').Router();
+const angelBeatsRoute = require('./angelBeatsRoute');
+const clannadRoute = require('./clannadRoute');
+const onePieceRoute = require('./onePieceRoute');
+const violetRoute = require('./violetEvergardenRoute');
+const loginRoute = require('./loginRoute');
+const logoutRoute = require('./logoutRoute');
+const signupRoute = require('./signupRoute');
+
+//router.use for all below
+router.use('/angelbeats', angelBeatsRoute);
+router.use('/clannad', clannadRoute);
+router.use('/onepiece', onePieceRoute);
+router.use('/violetevergarden', violetRoute);
+router.use('/login', loginRoute);
+router.use('/logout', logoutRoute);
+router.use('/signup', signupRoute);
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,12 @@
 //All routes below
 
-const router = require.apply('express').Router();
+const router = require('express').Router();
 const homeRoute = require('./homeRoute');
-const angelBeatsRoute = require('./api/angelBeatsRoute');
-const clannadRoute = require('./api/clannadRoute');
-const onePieceRoute = require('./api/onePieceRoute');
-const violetRoute = require('./api/violetEvergardenRoute');
+
 
 //router.use for all below
 
 router.use('/', homeRoute);
-router.use('/angelbeats', angelBeatsRoute);
-router.use('/clannad', clannadRoute);
-router.use('/onepiece', onePieceRoute);
-router.use('/violetevergarden', violetRoute);
+
 
 module.exports = router;
