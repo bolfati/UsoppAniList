c2280296192738b34f52c8f1261feabc929f6de8
Arthur Bridges <arthurbridges08@yahoo.com>
2023-12-05 19:58:43 -0600

    exporting routes

--- controllers/api/onePieceRoute.js
+++ controllers/api/onePieceRoute.js
@@ -11,16 +11,18 @@ const upload = multer({ dest: 'uploads/' });
 
 router.post('/onepiece', upload.single('file'), async (req, res) => {
   const { title, description, tags } = req.body;
-});
-const video = new Video({
-  title,
-  description,
-  tags,
-  url: req.file.path,
-  thumbnailUrl: 'placeholder',
-  duration: 0,
-});
 
-await video.save();
+  const video = new Video({
+    title,
+    description,
+    tags,
+    url: req.file.path,
+    thumbnailUrl: 'placeholder',
+    duration: 0,
+  });
 
-res.json(video);
+  await video.save();
+
+  res.json(video);
+});
+module.exports = router;
