9a544dea8c41209e6cd3779fc37b42d7bddba950
Arthur Bridges <arthurbridges08@yahoo.com>
2023-12-06 20:12:21 -0600

    seeded database

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,22 @@
 const sequelize = require('../config/connection');
-const { User, Anime, Comments } = require('../models');
-const userData = require('./userData.json');
-
+const seedAnime = require('./anime-seeds');
+const seedUsers = require('./user-seeds');
+const seedComments = require('./comments-seeds');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+  console.log('DATABASE SYNCED');
+
+  await seedUsers();
+  console.log('USERS SEEDED');
+
+  await seedAnime();
+  console.log('ANIME SEEDED');
+
+  await seedComments();
+  console.log('COMMENTS SEEDED');
+
   process.exit(0);
 };
-
-
 
 seedAll();
